d09f464c990a5ea2a7982502d3c387b09c63285b
Blitz17 <110586053+Blitz17@users.noreply.github.com>
2022-08-05 20:53:10 +0530

    Add files via upload

---
+++ Blood Bank Management/Blood_Bank/src/main/java/com/mycompany/blood_bank/Blood_Bank.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.blood_bank;
+
+/**
+ *
+ * @author User 01
+ */
+public class Blood_Bank {
+
+    public static void main(String[] args) {
+        new Blood_Bank_HomePage().setVisible(true);
+    }
+}
